f8dd19ca7aeee97afc6e127914c6bc4c8138407b
NicoloTartaggia <nicolo.tartaggia@gmail.com>
2020-04-29 14:16:39 +0200

    hw2 structure and exactDistance algorithm

--- Homeworks/BDC/src/main/java/G31HW2.java
+++ Homeworks/BDC/src/main/java/G31HW2.java
@@ -4,11 +4,12 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 
+// - Program must run correctly for pointsets belonging to Euclidean spaces of any dimensionality.
+//   Even if the datasets that we provided to test the program are in 2 dimensions, the program must work as well for datasets in R^d for arbitrary d.
+// - Running times of all methods must be expressed in milliseconds.
 public class G31HW2 {
 
     // Auxiliary methods
@@ -33,28 +34,58 @@ public class G31HW2 {
     }
 
     // Exact algorithm
-    public static int exactMPD(ArrayList<Vector> s) {
-
+    public static double exactMPD(ArrayList<Vector> s) {
+        double maxDistance = 0;
+        for (Vector v1 : s) {
+            for (Vector v2 : s) {
+                double currentDistance = Vectors.sqdist(v1, v2);
+                if (currentDistance > maxDistance) {
+                    maxDistance = currentDistance;
+                }
+            }
+        }
+        return maxDistance;
     }
 
-    // 2-approximation algorithm
-    public static int twoApproxMPD(ArrayList<Vector> s, int k) {
-
-    }
-
-    // k-center-based algorithm
-    public static ArrayList<Vector> kCenterMPD(ArrayList<Vector> s, int k) {
-
-    }
+    //// 2-approximation algorithm
+    //public static int twoApproxMPD(ArrayList<Vector> s, int k) {
+//
+    //}
+//
+    //// k-center-based algorithm
+    //public static ArrayList<Vector> kCenterMPD(ArrayList<Vector> s, int k) {
+//
+    //}
 
     public static void main(String[] args) throws IOException {
-        SparkConf conf = new SparkConf(true).setAppName("Homework2");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        sc.setLogLevel("WARN");
-
         // Reading points from a file whose name is provided as args[0]
         String filename = args[0];
+        int k = Integer.parseInt(args[1]);
         ArrayList<Vector> inputPoints = new ArrayList<>();
-        inputPoints = readVectorsSeq(filename);
+        inputPoints = readVectorsSeq(filename); // inputPoints = [[x1,y1], [x2,y2],..., [xn,yn]]
+
+        // Exact algorithm output
+        System.out.println("EXACT ALGORITHM");
+        long startTime1 = System.currentTimeMillis();
+        double exactMaxDistance1 = exactMPD(inputPoints);
+        System.out.println("Max distance = " + exactMaxDistance1 +
+                           "\nRunning time = " + (System.currentTimeMillis() - startTime1) + "\n");
+//
+        //// 2-approximation algorithm output
+        //System.out.println("2-APPROXIMATION ALGORITHM");
+        //long startTime2 = System.currentTimeMillis();
+        //int approxMaxDistance = twoApproxMPD(inputPoints, k);
+        //System.out.println("k" + k +
+        //                   "\nMax distance = " + approxMaxDistance +
+        //                   "\nRunning time = " + ((int)System.currentTimeMillis() - startTime2) + "\n");
+//
+        //// k-center-based algorithm output
+        //System.out.println("k-CENTER-BASED ALGORITHM");
+        //long startTime3 = System.currentTimeMillis();
+        //ArrayList<Vector> centers = kCenterMPD(inputPoints, k);
+        //int exactMaxDistance2 = exactMPD(centers);
+        //System.out.println("k" + k +
+        //                   "\nMax distance = " + exactMaxDistance2 +
+        //                   "\nRunning time = " + ((int)System.currentTimeMillis() - startTime3) + "\n");
     }
 }
