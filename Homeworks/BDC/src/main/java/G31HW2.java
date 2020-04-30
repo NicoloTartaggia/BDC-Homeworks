997589c2611c83276dfaef20c46d561a1c3b016b
NicoloTartaggia <nicolo.tartaggia@gmail.com>
2020-05-01 00:07:52 +0200

    Add first twoApprox algotirthm solution

--- Homeworks/BDC/src/main/java/G31HW2.java
+++ Homeworks/BDC/src/main/java/G31HW2.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Random;
 
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
@@ -11,6 +12,8 @@ import org.apache.spark.mllib.linalg.Vectors;
 //   Even if the datasets that we provided to test the program are in 2 dimensions, the program must work as well for datasets in R^d for arbitrary d.
 // - Running times of all methods must be expressed in milliseconds.
 public class G31HW2 {
+
+    private static final long SEED = 1237784;
 
     // Auxiliary methods
     public static Vector strToVector (String str) {
@@ -38,7 +41,10 @@ public class G31HW2 {
         double maxDistance = 0;
         for (Vector v1 : s) {
             for (Vector v2 : s) {
-                double currentDistance = Vectors.sqdist(v1, v2);
+                double currentDistance = Math.sqrt(Vectors.sqdist(v1, v2));
+                // double[] p1 = v1.toArray();
+                // double[] p2 = v2.toArray();
+                // double currentDistance = Math.sqrt(Math.pow((p1[0] - p2[0]), 2) + Math.pow((p1[1] - p2[1]), 2));
                 if (currentDistance > maxDistance) {
                     maxDistance = currentDistance;
                 }
@@ -47,14 +53,29 @@ public class G31HW2 {
         return maxDistance;
     }
 
-    //// 2-approximation algorithm
-    //public static int twoApproxMPD(ArrayList<Vector> s, int k) {
-//
-    //}
-//
-    //// k-center-based algorithm
+    // 2-approximation algorithm
+    public static double twoApproxMPD(ArrayList<Vector> s, int k) {
+        Random rand = new Random(); // Initialize random object
+        rand.setSeed(SEED); // Set its seed
+        ArrayList<Vector> s1 = new ArrayList<>();
+        for (int i = 0; i<k; i = i+1) {
+            s1.add(s.get(rand.nextInt(s.size())));  // Adding k random point from s to s1
+        }
+        double maxDistance = 0;
+        for (Vector v1 : s1) {
+            for (Vector v2 : s) {
+                double currentDistance = Math.sqrt(Vectors.sqdist(v1, v2));
+                if (currentDistance > maxDistance) {
+                    maxDistance = currentDistance;
+                }
+            }
+        }
+        return maxDistance;
+    }
+
+    // k-center-based algorithm
     //public static ArrayList<Vector> kCenterMPD(ArrayList<Vector> s, int k) {
-//
+
     //}
 
     public static void main(String[] args) throws IOException {
@@ -70,22 +91,22 @@ public class G31HW2 {
         double exactMaxDistance1 = exactMPD(inputPoints);
         System.out.println("Max distance = " + exactMaxDistance1 +
                            "\nRunning time = " + (System.currentTimeMillis() - startTime1) + "\n");
-//
-        //// 2-approximation algorithm output
-        //System.out.println("2-APPROXIMATION ALGORITHM");
-        //long startTime2 = System.currentTimeMillis();
-        //int approxMaxDistance = twoApproxMPD(inputPoints, k);
-        //System.out.println("k" + k +
-        //                   "\nMax distance = " + approxMaxDistance +
-        //                   "\nRunning time = " + ((int)System.currentTimeMillis() - startTime2) + "\n");
-//
+
+        // 2-approximation algorithm output
+        System.out.println("2-APPROXIMATION ALGORITHM");
+        long startTime2 = System.currentTimeMillis();
+        double approxMaxDistance = twoApproxMPD(inputPoints, k);
+        System.out.println("k = " + k +
+                           "\nMax distance = " + approxMaxDistance +
+                           "\nRunning time = " + (System.currentTimeMillis() - startTime2) + "\n");
+
         //// k-center-based algorithm output
         //System.out.println("k-CENTER-BASED ALGORITHM");
         //long startTime3 = System.currentTimeMillis();
         //ArrayList<Vector> centers = kCenterMPD(inputPoints, k);
         //int exactMaxDistance2 = exactMPD(centers);
-        //System.out.println("k" + k +
+        //System.out.println("k = " + k +
         //                   "\nMax distance = " + exactMaxDistance2 +
-        //                   "\nRunning time = " + ((int)System.currentTimeMillis() - startTime3) + "\n");
+        //                   "\nRunning time = " + (System.currentTimeMillis() - startTime3) + "\n");
     }
 }
