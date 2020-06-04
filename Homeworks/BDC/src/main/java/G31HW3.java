448a023068a2e3c6fef82493ea38078a9562cb84
Lorenzo Busin <lorenzo.busin@gmail.com>
2020-06-04 18:48:45 +0200

    first try runMapReduce()

--- Homeworks/BDC/src/main/java/G31HW3.java
+++ Homeworks/BDC/src/main/java/G31HW3.java
@@ -63,12 +63,75 @@ public class G31HW3 {
 
     } // END runSequential
 
+    // best k-clustering around these centers is the one where each point is assigned to the cluster of the closest center
+    // p: {pointset - s}, s: k centers
+    public static ArrayList<ArrayList<Vector>> Partition (ArrayList<Vector> p, ArrayList<Vector> s) {
+        ArrayList<ArrayList<Vector>> partitions = new ArrayList<>();  // each ArrayList<Vector> is a partition e contains its point
+        for (Vector center : s) { // initialize the partitions
+            ArrayList<Vector> partition = new ArrayList<>();
+            partition.add(center); // add the center as first element of the partition
+            partitions.add(partition);
+            p.remove(center); // remove the center from all points
+        }
+        for (Vector point : p) { // assign the points to the correct partition
+            double minDistance = Double.POSITIVE_INFINITY;
+            int centerIndex = 0;
+            for (int j = 0; j < s.size(); j++) {  // find the point with minimum distance
+                double currentDistance = Math.sqrt(Vectors.sqdist(point, s.get(j)));
+                if (currentDistance < minDistance) {
+                    minDistance = currentDistance;
+                    centerIndex = j;
+                }
+            }
+            partitions.get(centerIndex).add(point); // assing the point
+        }
+        return partitions;
+    }
+
+    // k-center-based algorithm
+    public static ArrayList<Vector> kCenterMPD(ArrayList<Vector> s, int k) {
+        ArrayList<Vector> c = new ArrayList<>(); // centers
+        int rand = (int) (Math.random() * s.size()); // random index for the first center selection
+        c.add(s.remove(rand));
+        for (int i = 0; i < (k - 1); i++){ // selects a center for each iteration
+            ArrayList<ArrayList<Vector>> currentPartitions = Partition(s, c); // assign the input points to the partitions
+            double maxDistance = 0;
+            Vector maxItem = null;
+            for (int l = 0; l < currentPartitions.size(); l++) { // find the center which is the point with max distance from its closest center
+                for (int j = 0; j < currentPartitions.get(l).size(); j++) {
+                    double currentDistance = Math.sqrt(Vectors.sqdist(c.get(l), currentPartitions.get(l).get(j)));
+                    if (currentDistance > maxDistance) {
+                        maxDistance = currentDistance;
+                        maxItem = currentPartitions.get(l).get(j);
+                    }
+                }
+            }
+            s.remove(maxItem); // remove the center from s
+            c.add(maxItem); // add the new center to c
+        }
+        return c;
+    }
+
     // f creates a Vector from a string representing its coordinates
-    public static Vector f(String point){}
+    public static Vector f(String point){
+        return Vectors.dense(Arrays.stream(point.split(",")).mapToDouble(Double::parseDouble).toArray());
+    }
 
-    public static ArrayList<Vector> runMapReduce(JavaRDD<Vector> pointsRDD, int k, int L) {}
+    public static ArrayList<Vector> runMapReduce(JavaRDD<Vector> pointsRDD, int k, int L) {
+        ArrayList<Vector> x = new ArrayList<>();
+        pointsRDD.mapPartitions((p) -> {
+            ArrayList<Vector> partition = new ArrayList<>(); // centers
+            while (p.hasNext()) {
+                //System.out.println(p.next());
+                partition.add(p.next());
+            }
+            System.out.println("Stop partition");
+            return kCenterMPD(partition, k).iterator();
+        }).collect();
+        return x;
+    }
 
-    public static int measure(ArrayList<Vector> pointsSet) {}
+    //public static int measure(ArrayList<Vector> pointsSet) {}
 
     public static void main(String[] args) throws IOException {
 
@@ -91,7 +154,7 @@ public class G31HW3 {
         JavaRDD<Vector> inputPoints = sc.textFile(inputPath).map(G31HW3::f).repartition(L).cache();
 
         System.out.println("Number of points = " + N +
-                           "\n k = " + k +
+                           "\nk = " + k +
                            "\nL = " + L +
                            "\nInitialization time = " + (System.nanoTime() - initializationTime)/1000000);
 
@@ -101,8 +164,8 @@ public class G31HW3 {
                            "Runtime of Round 2 = ");
 
         // Average distance
-        int averageDistance = measure(solution);
-        System.out.println("Average distance = " + averageDistance);
+        //int averageDistance = measure(solution);
+        //System.out.println("Average distance = " + averageDistance);
 
     }
 }
